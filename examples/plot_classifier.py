44a6e89320345720e8616e1963d01c4b8d5b5262
donlapark <donlapark@gmail.com>
2025-01-16 01:18:17 +0700

    add codecov

--- examples/plot_classifier.py
+++ examples/plot_classifier.py
@@ -11,7 +11,7 @@ An example plot of :class:`coverforest.CoverForestClassifier`
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from sklearn.datasets import make_classification
 
